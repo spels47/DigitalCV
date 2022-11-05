01d5e1d9a1a29984e2bbd0b1343e6dae5b4431ea
spels47 <patrickjoh33@hotmail.com>
2022-11-05 03:38:18 +0100

    improve image carousel

--- src/components/Dashboard/PictureCarousel.vue
+++ src/components/Dashboard/PictureCarousel.vue
@@ -1,83 +1,133 @@
 <template>
   <div class="d-flex justify-space-around align-center" style="max-height: 100%; overflow: hidden;">
-    <v-card v-if="!togglePatrick">
-      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="previousPicture" hide-delimiters>
-        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-      </v-carousel>
-    </v-card>
-    <v-card v-if="!togglePatrick">
-      <v-carousel style="width: 40vw;" height="35vh" v-model="selected" interval="10000" cycle progress progress-color="accent" hide-delimiters>
-        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-      </v-carousel>
-    </v-card>
-    <v-card v-if="!togglePatrick">
-      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="nextPicture" hide-delimiters>
-        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-      </v-carousel>
-    </v-card>
-
-    <!-- <v-card v-if="togglePatrick">
-      <v-carousel style="width: 500px;" height="350" :show-arrows="false" :value="previousPicture2" hide-delimiters>
+    <!-- <v-card>
+      <v-carousel style="width: 500px;" height="350" :show-arrows="false" :value="previousPicture" hide-delimiters>
         <v-carousel-item v-for="(item,i) in pictures" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card>
-    <v-card v-if="togglePatrick">
+    <v-card>
       <v-carousel style="width: 1000px;" height="550" v-model="selected" interval="10000" cycle progress progress-color="accent" hide-delimiters>
         <v-carousel-item v-for="(item,i) in pictures" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card>
-    <v-card v-if="togglePatrick">
-      <v-carousel style="width: 500px;" height="350" :show-arrows="false" :value="nextPicture2" hide-delimiters>
+    <v-card>
+      <v-carousel style="width: 500px;" height="350" :show-arrows="false" :value="nextPicture" hide-delimiters>
         <v-carousel-item v-for="(item,i) in pictures" :key="i" :src="item.src" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card> -->
 
-      <v-card v-if="togglePatrick">
-      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="previousPicture2" hide-delimiters>
-        <v-carousel-item src="@/assets/patrick1.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick2.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick3.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+      <v-card>
+      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="previousPicture" hide-delimiters>
         <v-carousel-item src="@/assets/patrick4.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick5.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick6.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick7.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick8.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick9.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick10.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick10.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
         <v-carousel-item src="@/assets/patrick11.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick12.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick13.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick14.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick15.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick16.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick17.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick18.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick19.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick20.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick21.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick22.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick23.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick24.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick25.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card>
-    <v-card v-if="togglePatrick">
+    <v-card>
       <v-carousel style="width: 40vw;" height="35vh" v-model="selected" interval="10000" cycle progress progress-color="accent" hide-delimiters>
-        <v-carousel-item src="@/assets/patrick1.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick2.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick3.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
         <v-carousel-item src="@/assets/patrick4.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick5.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick6.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick7.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick8.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick9.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick10.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick10.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
         <v-carousel-item src="@/assets/patrick11.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick12.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick13.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick14.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick15.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick16.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick17.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick18.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick19.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick20.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick21.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick22.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick23.jpg" contain max-height="35vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick24.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick25.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card>
-    <v-card v-if="togglePatrick">
-      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="nextPicture2" hide-delimiters>
-        <v-carousel-item src="@/assets/patrick1.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick2.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick3.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+    <v-card>
+      <v-carousel style="width: 20vw;" height="20vh" :show-arrows="false" :value="nextPicture" hide-delimiters>
         <v-carousel-item src="@/assets/patrick4.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick5.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick6.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick7.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick8.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick9.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick10.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick10.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
         <v-carousel-item src="@/assets/patrick11.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
-        <v-carousel-item src="@/assets/patrick12.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick13.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick14.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick15.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick16.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick17.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick18.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick19.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick20.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick21.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick22.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item reverse-transition="slide-x-reverse-transition" transition="slide-x-transition">
+          <v-img src="@/assets/patrick23.jpg" contain max-height="20vh"></v-img>
+        </v-carousel-item>
+        <v-carousel-item src="@/assets/patrick24.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
+        <v-carousel-item src="@/assets/patrick25.jpg" reverse-transition="slide-x-reverse-transition" transition="slide-x-transition"></v-carousel-item>
       </v-carousel>
     </v-card>
   </div>
@@ -98,20 +148,6 @@
     },
     data: function () {
       return {
-        items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
         pictures: [
         {
             src: '@/assets/patrick1.jpg',
@@ -151,7 +187,8 @@
           },
         ],
         selected: 0,
-        togglePatrick: true
+        togglePatrick: true,
+        pictureCount: 16
       }
     },
     methods: {
@@ -160,20 +197,11 @@
     computed: {
       previousPicture(){
         let previous = this.selected -1;
-        if(previous == -1) previous = this.items.length -1;
-        return previous % this.items.length;
+        if(previous == -1) previous = this.pictureCount -1;
+        return previous % this.pictureCount;
       },
       nextPicture(){
-        let next = (this.selected +1) % this.items.length;
-        return next;
-      },
-      previousPicture2(){
-        let previous = this.selected -1;
-        if(previous == -1) previous = this.pictures.length -1;
-        return previous % this.pictures.length;
-      },
-      nextPicture2(){
-        let next = (this.selected +1) % this.pictures.length;
+        let next = (this.selected +1) % this.pictureCount;
         return next;
       }
     },
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <div class="d-flex mb-4 justify-space-around" style="max-height: 50vh;">
       <PersonalInterestsList :loading="loading"></PersonalInterestsList>
 
-      <v-card :loading="loading" color="cardbg" class="mx-auto" :max-width="maxWidth" style="max-height: 100%; overflow-x: auto;">
+      <v-card :loading="loading" color="cardbg" class="mx-auto" :max-width="maxWidth" style="max-height: 100%; overflow-y: hidden;">
         <template slot="progress">
           <v-progress-linear color="accent" height="10" indeterminate></v-progress-linear>
         </template>
@@ -12,7 +12,7 @@
 
         <v-card-title>About Patrick</v-card-title>
 
-        <v-card-text>
+        <v-card-text style="max-height: 52%; overflow-y: auto;">
           Forære røbe anstrengende forgifte rundelige probat bekjenner, 
           studenterdagene åndedrag arvelige utstøtt, 
           smukt tyve prosenter oppsnuse reisepenge det konditoren. 
@@ -24,6 +24,50 @@
           deilige herefter skammelig. 
           Gledet sorgløs forestillet riktignok avkrok gasjen, 
           utspørre befordring bakhånden befordring dukkeseng.
+
+          <v-divider></v-divider>
+
+          Gjelden hjems hospitalslem, 
+          kontordøren passiar datere. 
+          Bet tarvelig hjems, 
+          betrygget bille gasjen åndedrag. 
+          Velsignede utenlandsreise besvær, 
+          forretningsplanen føye bekymringer tålte. 
+          Linde innvier er, 
+          forestillet utlandet værs'god elskverdig forfrossen. 
+          Ektemenn rekk bet og arkskrift, 
+          gasjen ekorne lerkefuglen.
+
+          <v-divider></v-divider>
+
+          Biomstendighetene punge erkjennelse Dem, 
+          måttet banksaker lerkefuglen småting besynderlig utlandet godhet. 
+          Råttenskap grassert begriper pakkene lenestolen forkler datum, 
+          dog ydmykende sås, 
+          hjerteløs dødsdag bekymringer luftdraget menneskers fastere. 
+          Forinnen især likesom utlandet kvartalsrenter enfoldig fordømme, 
+          handlings sunne børnene sunne. 
+          Formodentlig meg dukkeseng stivt gjemmespill moret, 
+          lengste dukkeseng forfrossen, 
+          avkrok formiddag middel. 
+          Har besynderlig fornøyelig pleie frelste innvendig fryde, 
+          således gnavet ekornet enfoldig.
+
+          <v-divider></v-divider>
+
+          Prøvet samtykker kan, 
+          undertiden pyntet innesto, 
+          innfall enkemann nedbrutt. 
+          Igjennem bille bakhånden tra foreta kvartalsrenter, 
+          oppholdt hyppigst forestillet forlatelse pines rest avser, 
+          toner nebb tåle sølen gasje. 
+          Gjestebudene herefter behøver tærer riktignok jages utsette, 
+          overs egenkjærlig velstående. 
+          Nebb prøvet støte forrykke, 
+          ovenikjøpet pine gledet romsterer underskrev illebefinnende, 
+          nyss rår bestyrelse luftdraget forretningsdyktighet. 
+          Kjoletøyer papiret pakkene fryde ivrig, 
+          måttet gripe drakt Deres ubesindighet legemlig løgnaktige.
         </v-card-text>
       </v-card>
 
