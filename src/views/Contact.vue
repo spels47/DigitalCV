08396b954a9bcca3b2cb00e947b27629d85460eb
spels47 <patrickjoh33@hotmail.com>
2022-11-06 19:45:03 +0100

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -14,10 +14,11 @@
         </v-card>
       </v-col>
       <v-col>
-        <v-card width="10vw" color="cardbg">
+        <v-card width="15vw" color="cardbg">
           <v-card-title class="title"><span class="pr-4">Contact info</span> <v-icon color="primary">mdi-email</v-icon></v-card-title>
           <div class="pa-8">
-            <v-text-field label="Phone number" color="secondary" prefix="+47" readonly @click="clickPhone()" :value="phone"></v-text-field>
+            <v-text-field label="Phone number" color="secondary" prefix="+47" persistent-hint :hint="copiedPhone ? 'copied to clipboard!' : ''" readonly @click="clickPhone()" :value="phone"></v-text-field>
+            <v-text-field label="Email" color="secondary" prefix="@" persistent-hint :hint="copiedEmail ? 'copied to clipboard!' : ''" readonly @click="clickEmail()" :value="email"></v-text-field>
           </div>
         </v-card>
       </v-col>
@@ -35,19 +36,36 @@
       return {
         subject: "",
         body: "",
-        phone: "98028998"
+        phone: "98028998",
+        email: "patrickjoh33@hotmail.com",
+        copiedEmail: false,
+        copiedPhone: false,
+        copyPhoneTimeout: null,
+        copyEmailTimeout: null,
       }
     },
     methods: {
       sendEmail(){
-        var email = "patrickjoh33@hotmail.com";
-        var link = `mailto:${email}?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(this.body)}`;
+        var link = `mailto:${this.email}?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(this.body)}`;
         console.log("link:", link);
         window.location.href = link;
       },
       clickPhone(){
+        this.copiedPhone = true;
+        clearTimeout(this.copyPhoneTimeout);
         navigator.clipboard.writeText(this.phone);
+        this.copyPhoneTimeout = setTimeout(() => {
+          this.copiedPhone = false;
+        }, 3000);
         window.location.href = "tel:" + this.phone
+      },
+      clickEmail(){
+        this.copiedEmail = true;
+        clearTimeout(this.copyEmailTimeout);
+        navigator.clipboard.writeText(this.email);
+        this.copyEmailTimeout = setTimeout(() => {
+          this.copiedEmail = false;
+        }, 3000);
       }
     },
     computed: {
